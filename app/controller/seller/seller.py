148fc69251577fcc6af12c184aa075536262f96b
yiwenjuan <ywj@gzhxyc.com>
2017-12-13 10:46:45 +0800

    配置路由

---
+++ app/controller/seller/seller.py
@@ -0,0 +1,199 @@
+# coding=utf-8
+# -*- coding: utf-8 -*-
+from flask import Blueprint
+model = Blueprint('seller', __name__)
+
+@model.route("/login/",methods=['POST'])
+def login():
+    """
+      @api {POST} /seller/login/ 01. 商家-登录
+      @apiGroup S_商家_Seller
+      @apiVersion 1.0.0
+
+      @apiPermission 访问授权
+
+      @apiParam {str} account 手机号码
+      @apiParam {str} [password] 密码
+      @apiSuccessExample {json} JSON.result 对象
+      {
+
+      }
+    """
+
+@model.route("/mine/logout/",methods=['POST'])
+def logout():
+    """
+      @api {POST} /seller/mine/logout/ 02. 商家-退出登录
+      @apiGroup S_商家_Seller
+      @apiVersion 1.0.0
+
+      @apiPermission 用户登录
+      @apiSuccessExample {json} JSON.result 对象
+      {
+
+      }
+    :return:
+    """
+
+@model.route("/my_clients/",methods=['POST'])
+def my_clients():
+    """
+       @api {POST} /seller/my_clients/ 03. 获取我的客户列表
+       @apiGroup S_商家_Seller
+       @apiVersion 1.0.0
+       @apiPermission 访问授权
+       @apiSuccessExample {json} JSON.result 对象
+       {
+         "users" : [
+                "_id": String # 主键，用户ID,其它地方为: user_id
+                "name": String # 姓名/昵称
+                "diet_timed" : Long #最新饮食日报发布时间，根据时间判断是否已报
+                "is_report" : Long #最新食谱发布时间，根据时间判断商家是否配餐
+            ]
+       }
+    :return:
+    """
+
+
+@model.route("/recipes/<string:user_id>/",methods=['POST'])
+def recipes():
+    """
+      @api {POST} /seller/recipes/<user_id>/ 04. 商家发布对应会员的食谱
+      @apiGroup S_商家_Seller
+      @apiVersion 1.0.0
+      @apiPermission 访问授权
+      @apiParam {str} user_id 用户ID
+      @apiParam {str} content 食谱内容
+      @apiSuccessExample {json} JSON.result 对象
+      {
+      }
+    :return:
+    """
+
+@model.route("/apply_clients/",methods=['POST'])
+def apply_clients():
+    """
+      @api {POST} /seller/apply_clients/ 05. 获取我的申请用户列表
+      @apiGroup S_商家_Seller
+      @apiVersion 1.0.0
+      @apiPermission 访问授权
+      @apiParam {str} next_start 分页起始ID 默认值: null
+      @apiSuccessExample {json} JSON.result 对象
+      {
+        apply_clients:[{
+           "_id": String # 主键，用户ID,其它地方为: user_id
+           "name": String # 姓名/昵称
+           "phone": String #联系方式
+        }]
+      }
+    :return:
+    """
+
+@model.route("/apply_clients_info/<string:user_id>/",methods=['POST'])
+def apply_clients_info(user_id):
+    """
+       @api {POST} /seller/apply_clients_info/<user_id>/ 06. 获取用户详细信息
+       @apiGroup S_商家_Seller
+       @apiVersion 1.0.0
+       @apiPermission 访问授权
+       @apiParam {str} user_id 用户ID
+       @apiSuccessExample {json} JSON.result 对象
+       {
+         "_id": String # 主键，用户ID,其它地方为: user_id
+         "name": String # 姓名/昵称
+         "phone": String #联系方式
+         "avatar": String # 头像
+         "sex": Int # 性别: [-1=未知/1=男/2=女]
+         "apply_weight" : Double #申请减重[单位：kg]
+         "estimated_times" : String #预计时间
+         "age": Int #年龄
+         "height" : Int #身高 [cm]
+         "weight" : Int #体重 [kg]
+         "sport" : String # 运动
+         "assessment" : String # 当前评估
+       }
+    :return:
+    """
+
+
+@model.route('/create_apply/<string:user_id>/',methods=['POST'])
+def create_apply(user_id):
+    """
+      @api {POST} /seller/create_apply/<user_id>/ 07. 操作用户申请
+      @apiGroup S_商家_Seller
+      @apiVersion 1.0.0
+      @apiPermission 访问授权
+      @apiParam {str} user_id 用户ID
+      @apiParam {bool} is_agree 是否同意[true=是/false=否]
+      @apiSuccessExample {json} JSON.result 对象
+      {
+      }
+    :return:
+    """
+
+@model.route('/user_recipes/<string:user_id>/')
+def user_recipes(user_id):
+    """
+       @api {GET} /seller/user_recipes/<user_id>/ 08. 获取用户食谱列表
+       @apiGroup S_商家_Seller
+       @apiVersion 1.0.0
+       @apiPermission 访问授权
+       @apiParam {str} user_id 用户ID
+       @apiParam {str} next_start 分页起始ID 默认值: null
+       @apiSuccessExample {json} JSON.result 对象
+       {
+         today_recipes:[
+            {
+                id: String # 食谱ID
+                content: String # 食谱内容
+                timed : Long 时间
+            }
+         ]
+       }
+    :return:
+    """
+
+@model.route('/dietetic_daily/<string:user_id>/')
+def dietetic_daily(user_id):
+    """
+     * @api {GET} /seller/dietetic_daily/<user_id>/ 09. 获取用户饮食日报列表
+     * @apiGroup S_商家_Seller
+     * @apiVersion 1.0.0
+     * @apiPermission 访问授权
+     * @apiParam {str} user_id 用户ID
+     * @apiParam {str} next_start 分页起始ID 默认值: null
+     * @apiSuccessExample {json} JSON.result 对象
+     * {
+     *   dietetic_daily:[
+     *      {
+     *          id: String # 饮食日报ID
+     *          timed : Long 时间
+     *      }
+     *   ]
+     * }
+    :return:
+    """
+
+@model.route('/comprehensive_daily/<string:user_id>/')
+def comprehensive_daily(user_id):
+    """
+       @api {GET} /seller/comprehensive_daily/<user_id>/ 10. 获取用户综合日报列表信息
+       @apiGroup U_用户_USER
+       @apiVersion 1.0.0
+       @apiPermission 访问授权
+       @apiParam {str} user_id 用户ID
+       @apiParam {str} next_start 分页起始ID 默认值: null
+       @apiSuccessExample {json} JSON.result 对象
+       {
+            "local_weight" : Double # 原始体重 [单位：kg]
+            "arrange_weight" : Double # 变化体重 [单位：kg]
+            "today_weight" : Double # 今日体重 [单位：kg]
+             "comprehensive" : [{
+                "today_weight": Double # 今日体重
+                "arrange_weight" : Double # 变化体重 [单位：kg]
+                "timed": Long # 创建时间
+             }]
+             "timed": Long # 创建时间
+       }
+    :return:
+    """
--- app/controller/user/user.py
+++ app/controller/user/user.py
@@ -3,45 +3,206 @@
 from flask import Blueprint, request, session, g, render_template
 model = Blueprint('user', __name__)
 
-#客户端用户列表
-@model.route("/user_list")
-def user_list():
-    水电费水电费
-    return "qqqq"
+@model.route("/oauth/<string:open_id>/",methods=['post'])
+def oauth(open_id):
+    """
+       @api {POST} /user/oauth/<open_id>/ 01. 用户-第三方账号登录
+       @apiGroup U_用户_USER
+       @apiVersion 1.0.0
 
-#客户端用户订单列表
-@model.route("/user_order_list")
-def user_order_list():
-    return render_template("user/user_order_list.html")
+       @apiPermission 访问授权
 
-#客户端用户订单详情
-@model.route("/order_info")
-def order_info():
-    return render_template("user/order_info.html")
+       @apiParam {str} open_id 第三方唯一ID
+       @apiParam {str} token 第三方授权TOKEN
+       @apiParam {long} expire 失效时间
+       @apiParam {str} name 第三方昵称
+       @apiParam {str} avatar 第三方头像
+       @apiParam {int} sex 第三方性别
+
+       @apiSuccessExample {json} JSON.result 对象
+       {
+
+       }
+    :return:
+    """
+
+@model.route("/today_recipes/")
+def today_recipes():
+    """
+       @api {GET} /user/today_recipes/ 02. 我的-食谱列表
+       @apiGroup U_用户_USER
+       @apiVersion 1.0.0
+       @apiPermission 访问授权
+       @apiParam {str} next_start 分页起始ID 默认值: null
+       @apiSuccessExample {json} JSON.result 对象
+       {
+         today_recipes:[
+            {
+                id: String # 食谱ID
+                content: String # 食谱内容
+                timed : Long 时间
+            }
+         ]
+       }
+    :return:
+    """
+
+@model.route("/dietetic_daily/")
+def dietetic_daily():
+    """
+     * @api {GET} /user/dietetic_daily/ 03. 我的-饮食日报列表
+     * @apiGroup U_用户_USER
+     * @apiVersion 1.0.0
+     * @apiPermission 访问授权
+     * @apiParam {str} next_start 分页起始ID 默认值: null
+     * @apiSuccessExample {json} JSON.result 对象
+     * {
+     *   dietetic_daily:[
+     *      {
+     *          id: String # 饮食日报ID
+     *          timed : Long 时间
+     *      }
+     *   ]
+     * }
+    :return:
+    """
 
 
-#客户端用户详细信息
-@model.route("/user_info")
-def user_info():
-    return render_template("user/user_info.html")
+@model.route("/create/dietetic_daily/",methods=['post'])
+def dietetic_daily():
+    """
+       @api {POST} /user/create/dietetic_daily/ 04. 用户发布饮食日报信息
+       @apiGroup U_用户_USER
+       @apiVersion 1.0.0
+       @apiPermission 访问授权
+       @apiParam {int} type 类别：[0=早餐/1=午餐/2=晚餐]
+       @apiParam {str} content 内容
+       @apiParam {list} images 饮食图片资料数组
+       @apiSuccessExample {json} JSON.result 对象
+       {
+       }
+    :return:
+    """
 
-#客户端用户状态标记view
-@model.route("/mark")
-def mark():
-    return '1'
+@model.route("/get/dietetic_daily/<string:diet_id>/")
+def dietetic_daily(diet_id):
+    """
+       @api {GET} /user/get/dietetic_daily/<diet_id>/ 05. 我的-发布饮食日报详细信息
+       @apiGroup U_用户_USER
+       @apiVersion 1.0.0
+       @apiPermission 访问授权
+       @apiParam {str} diet_id 饮食日报ID
+       @apiSuccessExample {json} JSON.result 对象
+       {
+             "dietetic" : [{
+                "content": String # 内容
+                "images": [  # 饮食图片资料数组
+                    {
+                    "url": String # 图片地址
+                    "ratio": Float # 图片宽高比
+                    }
+                    ]
+                "type": Int # 日报类别: [0=早餐/1=午餐/2=晚餐]
+                "timed": Long # 餐饮时间
+             }]
+             "timed": Long # 创建时间
+       }
+    :return:
+    """
 
-#提交状态标记
-@model.route("/submit_mark")
-def submit_mark():
-    return '1'
+@model.route("/get/comprehensive_daily/")
+def comprehensive_daily():
+    """
+       @api {GET} /user/get/comprehensive_daily/ 06. 我的-获取综合日报列表信息
+       @apiGroup U_用户_USER
+       @apiVersion 1.0.0
+       @apiPermission 访问授权
+       @apiParam {str} next_start 分页起始ID 默认值: null
+       @apiSuccessExample {json} JSON.result 对象
+       {
+            "local_weight" : Double # 原始体重 [单位：kg]
+            "arrange_weight" : Double # 变化体重 [单位：kg]
+            "today_weight" : Double # 今日体重 [单位：kg]
+             "comprehensive" : [{
+                "today_weight": Double # 今日体重
+                "arrange_weight" : Double # 变化体重 [单位：kg]
+                "timed": Long # 创建时间
+             }]
+             "timed": Long # 创建时间
+       }
+    :return:
+    """
 
 
-#所有用户订单列表
-@model.route('/all_order_list')
-def all_order_list():
-    return render_template('user/user_order_list.html')
+@model.route('/get/comprehensive_info/<string:comprehensive_id>/')
+def comprehensive_info(comprehensive_id):
+    """
+     * @api {GET} /user/get/comprehensive_info/<comprehensive_id>/ 07. 获取综合日报详细信息
+     * @apiGroup U_用户_USER
+     * @apiVersion 1.0.0
+     * @apiPermission 访问授权
+     * @apiParam {str} comprehensive_id 用户ID
+     * @apiSuccessExample {json} JSON.result 对象
+     * {
+     *       "_id": String # 主键，综合日报ID,
+     *       "weight": String # 今日体重
+     *       "waist": String # 今日腰围
+     *       "images":   # 今日运动量
+     *           {
+     *           "url": String # 图片地址
+     *           "ratio": Float # 图片宽高比
+     *           }
+     *       "timed": Long # 创建时间
+     * }
+    :return:
+    """
 
-#订单明细
-@model.route('/orderinfo')
-def orderinfo():
-    return render_template('user/order_info.html')
+@model.route('/obesity_test/',mthods=['post'])
+def obesity_test():
+    """
+     * @api {POST} /user/obesity_test/ 08. 肥胖测试
+     * @apiGroup U_用户_USER
+     * @apiVersion 1.0.0
+     * @apiPermission 访问授权
+     * @apiParam {int} sex 性别
+     * @apiParam {int} age 年龄
+     * @apiParam {int} height 身高
+     * @apiParam {double} weight 体重
+     * @apiParam {str} sport 运动量
+     * @apiSuccessExample {json} JSON.result 对象
+     * {
+     *  ...
+     * }
+    :return:
+    """
+
+@model.route('/apply_free_consultation/',methods=['post'])
+def apply_free_consultation():
+    """
+     * @api {POST} /user/apply_free_consultation/ 09. 申请免费咨询
+     * @apiGroup U_用户_USER
+     * @apiVersion 1.0.0
+     * @apiPermission 访问授权
+     * @apiParam {int} sex 性别
+     * @apiParam {int} age 年龄
+     * @apiParam {int} height 身高
+     * @apiParam {double} weight 体重
+     * @apiParam {str} sport 运动量
+     * @apiSuccessExample {json} JSON.result 对象
+     * {
+     * }
+    :return:
+    """
+
+@model.route('/files_upload/',methods=['post'])
+def files_upload():
+    """
+     * @api {POST} /user/files_upload/ 10. 文件上传接口
+     * @apiGroup U_用户_USER
+     * @apiVersion 1.0.0
+     * @apiPermission 访问授权
+     * @apiSuccessExample {json} JSON.result 对象
+     * {
+     * }
+    :return:
+    """
