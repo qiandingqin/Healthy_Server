e29e5c3bce6789ff2333cf936913c83485d189e5
yiwenjuan <ywj@gzhxyc.com>
2017-12-13 14:50:12 +0800

    添加common

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@
 from flask import Flask
 from controller.index import model as index
 from controller.user import model as user
+from controller.seller import model as seller
 
 app = Flask(__name__)
 app.register_blueprint(index, )
 app.register_blueprint(user, )
+app.register_blueprint(seller,)
