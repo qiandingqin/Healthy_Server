3f3bb91fee7e89ce20f8afcc375d95fca8b0a703
邹海林 <1586905673@qq.com>
2017-12-13 13:43:32 +0800

    测试配置

--- app/controller/index/index.py
+++ app/controller/index/index.py
@@ -13,8 +13,8 @@ import time
 model = Blueprint('index', __name__)
 
 @model.route('/')
-def index():
-    uri = urlencode({"url": "http://www.dnanren.cn/index"})
+def put_code():
+    uri = urlencode({"url": "http://www.dnanren.cn/indexs"})
     url_tpl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + str(appID) + "&redirect_uri=" + uri[4:-1] + "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect"
     return "<script>window.location.href='"+url_tpl+"'</script>"
 
@@ -24,6 +24,7 @@ def index():
     token_url = requests.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid="+appID+"&secret="+appsecret+"&code="+code+"&grant_type=authorization_code");
     json_lode = json.loads(token_url.content)
     openid = json_lode["openid"]
+
     access_token = json_lode["access_token"]
     user_info = requests.get("https://api.weixin.qq.com/sns/userinfo?access_token="+access_token+"&openid="+openid+"&lang=zh_CN")
     json_user_info = json.loads(user_info.content)
