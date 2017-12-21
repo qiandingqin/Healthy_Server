120e8385dd828372e40cba902d57f7165da5da6d
邹海林 <1586905673@qq.com>
2017-12-21 10:22:24 +0800

    222

--- app.py
+++ app.py
@@ -11,7 +11,19 @@ app.config.from_object(config)
 
 @app.errorhandler(404)
 def not_found(e):
-    return redirect(url_for("user.not_founts"))
+    if "user_id" in session:
+        user_id = session["user_id"]
+        if user_id == None:
+            return render_template("not_found.html")
+        else:
+            user_info = config.DB.users.find_one({"_id": user_id})
+            if user_info != None:
+                if user_info["type"] == 1:
+                    return render_template("index/index_admin.html")
+                else:
+                    return render_template("index/index.html")
+    else:
+        return render_template("not_found.html")
 
 # @app.errorhandler(500)
 # def internal_server_error(e):
